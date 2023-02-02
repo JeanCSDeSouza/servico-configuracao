cdb5f914d7aa839d0388bd0a45518c76d1041c54
Jean Carlos <jcsouza.first@gmail.com>
2023-02-02 06:45:30 -0300

    jdbc profile
    Implementação do jdbc backend usando mysql

--- src/main/java/br/com/livrariareal/servicoconfiguracao/ServicoConfiguracaoApplication.java
+++ src/main/java/br/com/livrariareal/servicoconfiguracao/ServicoConfiguracaoApplication.java
@@ -2,15 +2,14 @@ package br.com.livrariareal.servicoconfiguracao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigServer
 @EnableDiscoveryClient
 public class ServicoConfiguracaoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServicoConfiguracaoApplication.class, args);
 	}
